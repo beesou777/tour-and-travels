24d46de4256e635d3a22797ec398a989998d0e37
beesou777 <sbeesou@gmail.com>
2024-09-19 17:29:01 +0545

    fix : leaflet icon url fixed

--- components/Tour/MapDisplay.vue
+++ components/Tour/MapDisplay.vue
@@ -6,9 +6,9 @@
       style="height: 90vh; width: 100%; border-radius: 12px"
     >
       <l-tile-layer :url="url" />
-        <l-marker :lat-lng="center">
-          <l-icon :icon-size="[21, 21]" class="markers">{{ props.selectedTour?.price }}</l-icon>
-        </l-marker>
+      <l-marker :lat-lng="center" :icon="defaultIcon">
+        <l-icon :icon-size="[21, 21]" class="markers">{{ props.selectedTour?.price }}</l-icon>
+      </l-marker>
       <l-marker
         v-if="selectedTour"
         :lat-lng="[selectedTour.lat, selectedTour.lng]"
@@ -19,14 +19,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch } from "vue";
 import "leaflet/dist/leaflet.css";
+
 const props = defineProps<{
   selectedTour: {
     lat: number;
     lng: number;
     img: string;
     title: string;
+    price: number; // Ensure price is included
   } | null;
 }>();
 
@@ -34,39 +35,37 @@ const zoom = ref(13);
 const center = ref([47.41322, -1.219482]);
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
-let defaultIcon = null;
+// Define defaultIcon
+const defaultIcon = computed(() =>
+  L.icon({
+    iconUrl: "path/to/default-icon.png", // Ensure this path is correct
+    iconSize: [32, 32],
+  })
+);
 
 // Define imageIcon as divIcon
-let imageIcon = null;
-if (process.client) {
-  defaultIcon = computed(() => {
-    L.icon({
-      iconUrl: "path/to/default-icon.png", // Provide a valid default icon URL
-      iconSize: [32, 32], // Adjust the size as needed
-    });
-  });
+const imageIcon = computed(() =>
+  L.divIcon({
+    className: "custom-icon",
+    html: `
+    <div class="bg-white rounded-md p-3 w-full min-w-[200px] relative -translate-y-[200px] -translate-x-[60px] before:content-[''] before:absolute before:h-[10px] before:w-[20px] before:bg-white before:[clip-path:polygon(45%_100%,_0_0,_100%_0)] before:bottom-[-10px] before:left-[50%] before:translate-x-[-50%] [box-shadow:8px_1px_10px_0px_rgba(0,_0,_0,_0.08)]">
+      <span class="flex justify-center items-center w-8 h-8 absolute top-[-10px] right-[-10px] h3 bg-gray-200 rounded-full cursor-pointer">
+        x
+      </span>
+      <img src="${props.selectedTour?.img}" style="width: 100%; height: 100px;"/>
+      <p class="font-bold py-2">${props.selectedTour?.title}</p>
+      <p class="body-bold text-primary">$${props.selectedTour?.price}</p>
+    </div>
+  `,
+  })
+);
 
-  imageIcon = computed(() =>
-    L.divIcon({
-      className: "custom-icon",
-      html: `
-      <div class="bg-white rounded-md p-3 w-full min-w-[200px] relative -translate-y-[200px] -translate-x-[60px] before:content-[''] before:absolute before:h-[10px] before:w-[20px] before:bg-white before:[clip-path:polygon(45%_100%,_0_0,_100%_0)] before:bottom-[-10px] before:left-[50%] before:translate-x-[-50%] [box-shadow:8px_1px_10px_0px_rgba(0,_0,_0,_0.08)]">
-        <span class="flex justify-center items-center w-8 h-8 absolute top-[-10px] right-[-10px] h3 bg-gray-200 rounded-full cursor-pointer">
-          x
-        </span>
-        <img src="${props.selectedTour?.img}" style="width: 100%; height: 100px;"/>
-        <p class="font-bold py-2">${props.selectedTour?.title}</p>
-        <p class="body-bold text-primary">$${props.selectedTour?.price}</p>
-      </div>
-    `,
-    })
-  );
-}
+
 
 // Watch selectedTour prop to update the map center
 watch(
   () => props.selectedTour,
-  (newTour: any) => {
+  (newTour) => {
     if (newTour) {
       center.value = [newTour.lat, newTour.lng];
     }
